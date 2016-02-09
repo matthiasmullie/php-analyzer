dc82c289623c296fd07710e567774bdcba9f576c
Matthias Mullie <git@mullie.eu>
2016-02-09 22:35:24 +0100

    Skip analyzer test if server isn't running
    The built-in PHP server won't be up when running tests on HHVM...

--- tests/AnalyzerTest.php
+++ tests/AnalyzerTest.php
@@ -4,10 +4,13 @@ namespace Cauditor\Tests;
 
 use Cauditor\Analyzer;
 use Cauditor\Config;
+use Cauditor\Exception;
 use PHPUnit_Framework_TestCase;
 
 class AnalyzerTest extends PHPUnit_Framework_TestCase
 {
+    protected $api = 'http://localhost:8000';
+
     protected $reader;
 
     /**
@@ -16,21 +19,39 @@ class AnalyzerTest extends PHPUnit_Framework_TestCase
      * is running, e.g. like this:
      * `php -S localhost:8000 -t tests/analyze`.
      *
-     * @throws \Cauditor\Exception
+     * @throws Exception
      */
     public function testAnalyzer()
     {
+        if (!$this->isServerAvailable()) {
+            $this->markTestSkipped('Local server is not running.');
+        }
+
         $path = __DIR__.'/../vendor/matthiasmullie/php-skeleton';
         $config = __DIR__.'/analyze/php-skeleton.yml';
 
         $config = new Config($path, $config);
         $analyzer = new Analyzer($config);
-        $result = $analyzer->run('http://localhost:8000/api.php');
+        $result = $analyzer->run($this->api.'/api.php');
 
         $expect = file_get_contents(__DIR__.'/build/cauditor.json');
         // generated JSON file
         $this->assertEquals($expect, file_get_contents(__DIR__.'/analyze/tmp/cauditor.json'));
         // submitted to API
         $this->assertEquals($expect, $result);
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isServerAvailable()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->api);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        return $result !== false;
     }
 }
