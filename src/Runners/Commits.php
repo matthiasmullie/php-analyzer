7f646859796297059ae9e4396ebfd21e4a66e9d1
Matthias Mullie <git@mullie.eu>
2016-03-02 16:15:34 +0100

    Add runner for specific commits

---
+++ src/Runners/Commits.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Cauditor\Runners;
+
+use Cauditor\Analyzers\AnalyzerInterface;
+use Cauditor\Api;
+use Cauditor\Config;
+
+/**
+ * @author Matthias Mullie <cauditor@mullie.eu>
+ * @copyright Copyright (c) 2016, Matthias Mullie. All rights reserved.
+ * @license LICENSE MIT
+ */
+class Commits extends All implements RunnerInterface
+{
+    /**
+     * @var array
+     */
+    protected $commits = array();
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param array $commits
+     */
+    public function __construct(Config $config, Api $api, AnalyzerInterface $analyzer, array $commits)
+    {
+        parent::__construct($config, $api, $analyzer);
+
+        $this->commits = $commits;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getCommits()
+    {
+        return $this->commits;
+    }
+}
